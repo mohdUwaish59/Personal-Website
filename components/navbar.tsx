030af9df8ba0d0dc13410410f75b89585038d4f7
mohdUwaish59 <mohd.uwaish.e21@nsut.ac.in>
2025-03-01 15:45:43 +0100

    Fix: Fixing Navbar links state

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -49,7 +49,7 @@ export function Navbar() {
             whileHover={{ scale: 1.05 }}
             transition={{ type: "spring", stiffness: 400, damping: 10 }}
           >
-            Michael<span className="text-primary">Johnson</span>
+            Mohd<span className="text-primary">Uwaish</span>
           </motion.div>
         </Link>
 
